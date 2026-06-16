30366a63061505ebe916e38359773ce23f7548c3
saeleuterio <saeleuterio@hotmail.com>
2026-06-16 15:45:43 -0300

    up

---
+++ src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { BotaoComponent } from "../../../compartilhados/botao/botao.component";
+
+@Component({
+  selector: 'app-botao-adicionar-transacao',
+  imports: [BotaoComponent],
+  templateUrl: './botao-adicionar-transacao.component.html',
+  styleUrl: './botao-adicionar-transacao.component.css'
+})
+export class BotaoAdicionarTransacaoComponent {
+
+}
