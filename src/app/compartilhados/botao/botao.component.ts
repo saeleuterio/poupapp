c27ab4076c3bc672d7977c2e7911ede496b9ac51
saeleuterio <saeleuterio@hotmail.com>
2026-06-16 09:45:35 -0300

    up

---
+++ src/app/compartilhados/botao/botao.component.ts
@@ -0,0 +1,9 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-botao',
+  imports: [],
+  templateUrl: './botao.component.html',
+  styleUrl: './botao.component.css',
+})
+export class BotaoComponent {}
