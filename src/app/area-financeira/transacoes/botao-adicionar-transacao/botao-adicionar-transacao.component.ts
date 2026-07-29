c951863147486a585f8aee3e39784d9985fad23e
saeleuterio <saeleuterio@hotmail.com>
2026-07-29 09:19:45 -0300

    up

--- src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
+++ src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal, effect } from '@angular/core';
 import { BotaoComponent } from "../../../compartilhados/botao/botao.component";
+import { ModalComponent } from "../../../compartilhados/modal/modal.component";
 
 @Component({
   selector: 'app-botao-adicionar-transacao',
-  imports: [BotaoComponent],
+  imports: [BotaoComponent, ModalComponent],
   templateUrl: './botao-adicionar-transacao.component.html',
   styleUrl: './botao-adicionar-transacao.component.css'
 })
+abrirModal() {
+throw new Error('Method not implemented.');
+}
 export class BotaoAdicionarTransacaoComponent {
+  modalAberto = signal(false);
 
+  abrirModal() {
+    this.modalAberto.set(true);
+  }
 }
